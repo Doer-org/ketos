969933022bfa811ca9421a60fd699eaf37ab9ac8
clcl777 <77223796+clcl777@users.noreply.github.com>
2024-02-04 17:44:51 +0900

    sendTar追加

--- cmd/create.go
+++ cmd/create.go
@@ -27,6 +27,7 @@ var createCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		createImageWithPack()
 		compressImageToTar()
+		sendTarToServer()
 	},
 }
 
@@ -69,6 +70,12 @@ func compressImageToTar() {
     if err != nil {
         panic(err)
     }
+}
+
+func sendTarToServer() {
+	// tarをサーバに送信
+	// TODO: サーバに送信する処理を実装
+	// TODO: サーバに送信したらtarを削除する
 }
 
 func init() {
