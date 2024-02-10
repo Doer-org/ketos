e7b167654ab13f58e67e3c825bf5d88e1a98dba0
clcl777 <77223796+clcl777@users.noreply.github.com>
2024-02-10 13:44:18 +0900

    add test

--- internal/docker/receive/run_docker_container_test.go
+++ internal/docker/receive/run_docker_container_test.go
@@ -1,1 +1,16 @@
 package docker
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRunContainer(t *testing.T){
+	respId, err := CreateContainer()
+	assert.NotEmpty(t, respId)
+	t.Logf("respId: %s", respId)
+	assert.NoError(t, err)
+	err = RunContainer(respId)
+	assert.NoError(t, err)
+}
