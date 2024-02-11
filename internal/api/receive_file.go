0fb502d904183e6b4f315885c89adb3acace4264
clcl777 <77223796+clcl777@users.noreply.github.com>
2024-02-11 12:55:17 +0900

    port自動で指定

--- internal/api/receive_file.go
+++ internal/api/receive_file.go
@@ -2,61 +2,69 @@ package api
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 )
 
-func GetServerInfo(id string) error {
+type Response struct {
+    Port string `json:"port"`
+}
+
+func GetServerInfo(id string) (string,error) {
 	fullURL := fmt.Sprintf("%s/info/%s", BackendURL, id)
 	response, err := http.Get(fullURL)
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer response.Body.Close()
 
 	var responseBody bytes.Buffer
 	_, err = io.Copy(&responseBody, response.Body)
 	if err != nil {
-		return err
+		return "", err
 	}
-	fmt.Println(fmt.Sprintf("%s/info/%s", FrontURL, id))
-	fmt.Println(string(responseBody.String()))
 
-	return nil
+	var resp Response
+	err = json.Unmarshal(responseBody.Bytes(), &resp)
+	if err != nil {
+		return "", err
+	}
+	fmt.Printf(fmt.Sprintf("%s/info/%s", FrontURL, id))	
+	return resp.Port, nil
 }
 
-func ReceiveTarGzFromServer(id string) error {
+func ReceiveTarGzFromServer(id string) (string, error) {
 	fullURL := fmt.Sprintf("%s/%s", BackendURL, id)
 	response, err := http.Get(fullURL)
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer response.Body.Close()
 
 	infoURL := fmt.Sprintf("%s/info/%s", BackendURL, id)
 	infoResponse, err := http.Get(infoURL)
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer infoResponse.Body.Close()
 
-	err = GetServerInfo(id)
+	port, err := GetServerInfo(id)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	file, err := os.Create(filePath)
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer file.Close()
 
 	_, err = io.Copy(file, response.Body)
 	if err != nil {
-		return err
+		return "", err
 	}
-
-	return nil
+	return port, nil
 }
--- internal/docker/pull/create_docker_container.go
+++ internal/docker/pull/create_docker_container.go
@@ -3,26 +3,52 @@ package docker
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/Doer-org/ketos/internal/docker"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
 )
 
-func CreateContainer() (string, error) {
+func CreateContainer(port string) (string, error) {
 	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		return "", err
 	}
 
+	// ポートのマッピングを解析する
+	portMappings := strings.Split(port, ":")
+	if len(portMappings) != 2 {
+		return "", fmt.Errorf("invalid port mapping format")
+	}
+	hostPort := portMappings[0]
+	containerPort := portMappings[1] + "/tcp"
+
+	// PortBindingsの設定
+	portBindings := nat.PortMap{
+		nat.Port(containerPort): []nat.PortBinding{
+			{
+				HostIP:   "0.0.0.0",
+				HostPort: hostPort,
+			},
+		},
+	}
+
 	// コンテナを作成
 	resp, err := cli.ContainerCreate(ctx, &container.Config{
-		Image: docker.ImageName,
-	}, nil, nil, nil, docker.ContainerName)
+		Image:        docker.ImageName,
+		ExposedPorts: nat.PortSet{nat.Port(containerPort): struct{}{}},
+	}, &container.HostConfig{
+		PortBindings: portBindings,
+	}, nil, nil, docker.ContainerName)
 	if err != nil {
 		return "", err
 	}
-	fmt.Printf("Container has been created\n")
+	if err != nil {
+		return "", err
+	}
+	fmt.Printf("Container has been created with ID: %s\n", resp.ID)
 	return resp.ID, nil
 }
